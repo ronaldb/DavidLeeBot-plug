438d2f89ec39b475a9503db7e28326f33d9097ee
Ronald Bruintjes <ronaldb64@gmail.com>
2014-04-06 11:35:00 -0400

    Make meh only admin accessible.

--- commands/meh.js
+++ commands/meh.js
@@ -9,16 +9,19 @@ exports.handler = function(data) {
                      "This is horrible...",
                      "Oh great... now I'll have nightmares."];
     var response = myutils.pickone(responses);
-    if (currentsong.song == null) {
-    	response = "There's nothing playing!";
+
+    if (admincheck(data.userid)) {
+        if (currentsong.song == null) {
+            response = "There's nothing playing!";
+        }
+        else if (currentsong.mehed) {
+            response = "I know it's bad, but I can only hate it so much...";
+        }
+        else {
+            currentsong.wooted = false;
+            currentsong.mehed  = true;
+            bot.meh();
+        }
+        output({text: response, destination: data.source, userid: data.userid});
     }
-    else if (currentsong.mehed) {
-        response = "I know it's bad, but I can only hate it so much...";
-    }
-    else {
-        currentsong.wooted = false;
-        currentsong.mehed  = true;
-	    bot.meh();
-    }
-    output({text: response, destination: data.source, userid: data.userid});
 }
