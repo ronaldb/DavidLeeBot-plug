befe2f1de69a578564d7eb14380d78266dcf7aa5
Ronald Bruintjes <ronaldb64@gmail.com>
2017-02-26 22:55:18 -0500

    French stuff
    For some reason, the people in the room suspected DLB was being turned into a French sex bot. Added some French responses....

--- commands/woot.js
+++ commands/woot.js
@@ -11,15 +11,33 @@ exports.handler = function(data) {
                      ":notes: :notes: I can hear this song in my sleep.",
                      "Love this!",
                      "Rock on!"];
+    var responseFrance = ["C'est tres bon!",
+                          "Cette chanson est génial!",
+                          "Yeah! Je ne peux pas arrêter de danser à cela!",
+                          ":notes: :notes: Je peux entendre dans mon sommeil cette chanson.",
+                          "Je peux le creuser"];
     var response = myutils.pickone(responses);
+    console.log(data.name);
+    if (data.command == '.lewoot') {
+        response = myutils.pickone(responseFrance);
+    }
     if (currentsong.song == null) {
-    	response = "There's nothing playing!";
+        if (data.command == '.lewoot')
+            response = "Il n'y a rien à jouer!";
+        else
+    	    response = "There's nothing playing!";
     }
     else if (data.userid == currentsong.djid) {
-    	response = "You can't woot for yourself!";
+        if (data.command == '.lewoot')
+            response = "Vous ne pouvez pas woot pour vous-même!";
+        else
+    	    response = "You can't woot for yourself!";
     }
     else if (currentsong.wooted) {
-        response = "I can't woot any louder!";
+        if (data.command == '.lewoot')
+            response = "Je ne peux pas woot plus fort!";
+        else
+            response = "I can't woot any louder!";
     }
     else {
         currentsong.wooted = true;
